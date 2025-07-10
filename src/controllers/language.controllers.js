f11ce973609a166ff6fa77a4281a8b3e85272d99
NatZigaran. <natanaelzigaran@gmail.com>
2025-07-10 16:20:51 -0600

    Se realizaron cambios en controladores

--- src/controllers/language.controllers.js
+++ src/controllers/language.controllers.js
@@ -31,7 +31,7 @@ export const createPrograms = async (req, res)=>{
         if(!Number.isInteger(release_year)) return res.status(400).json({messag: "el año de lanzamiento debe ser un entero"})
         if(release_year <= 0) return res.status(400).json({message: "el año debe ser mayor a 0"})
         const buscador = await model.findOne(req.params,{where:{name:req.params.name}})
-        if(buscador) return res.status(400).json({message:"El ya nombre existe, el nombre debe ser unico"})
+        if(buscador) return res.status(400).json({message:"El nombre ya existe, el nombre debe ser unico"})
         const crear = await model.create(req.body)
         return res.status(201).json(crear);
     } catch (error) {
@@ -49,22 +49,20 @@ export const deleteProgram = async (req, res)=>{
     }
 }
 export const updateProgram = async (req, res)=>{
-        const {name, paradigm, release_year } = req.body
     try {
-
         if(name===undefined || name === "") return res.status(400).json({message:" name es obligatorio"})
         if(paradigm === undefined || paradigm === "") return res.status(400).json({message:"paradigma es obligatorio"})
         if(!Number.isInteger(release_year)) return res.status(400).json({messag: "el año de lanzamiento debe ser un entero"})
         if(release_year <= 0) return res.status(400).json({message: "el año debe ser mayor a 0"})
         const buscador = await model.findOne(req.params,{where:{name:req.params.name}})
-        if(buscador) return res.status(400).json({message:"El ya nombre existe, el nombre debe ser unico"})
-
+        if(buscador) return res.status(400).json({message:"El nombre ya existe, el nombre debe ser unico"})
+        
         const [actualizar] = await model.update(req.body,{where:{id: req.params.id}})
-        if (actualizar) {const encontro = await model.findByPk(req.params.id)} return res.status(200).json(encontro)
-
+        if(actualizar){const encontro = await model.findByPk(req.params.id)
+            return res.status(200).json(encontro)
+        }else{return res.status(404).json({message:"No se encontro el id para modificar"})}
+        
     } catch (error) {
-
         return res.status(400).json({message:"Error al tratar de actualizar un lenguaje"})
-
     }
 }
