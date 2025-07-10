98901480fe29d665db2313516393985adfeda601
NatZigaran. <natanaelzigaran@gmail.com>
2025-07-10 14:50:42 -0600

    se trabajo en los controladores, pero no se realizo las crud aun ni las verificaciones

---
+++ src/controllers/language.controllers.js
@@ -0,0 +1,37 @@
+import model from "../models/language.model.js";
+
+export const getAllPrograms = async ()=>{
+    try {
+        
+    } catch (error) {
+        return res.status(400).json({message:"Error al tratar de traer todos los lenguajes"})
+    }
+}
+export const getProgramsById = async ()=>{
+    try {
+        
+    } catch (error) {
+        return res.status(400).json({message:"Error al tratar de traer todos los lenguajes"})
+    }
+}
+export const createPrograms = async ()=>{
+    try {
+        
+    } catch (error) {
+        return res.status(400).json({message:"Error al tratar de traer todos los lenguajes"})
+    }
+}
+export const deleteProgram = async ()=>{
+    try {
+        
+    } catch (error) {
+        return res.status(400).json({message:"Error al tratar de traer todos los lenguajes"})
+    }
+}
+export const updateProgram = async ()=>{
+    try {
+        
+    } catch (error) {
+        return res.status(400).json({message:"Error al tratar de traer todos los lenguajes"})
+    }
+}
